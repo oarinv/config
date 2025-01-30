554bfcd9f415399b20f801aaa1ac3be3f55c3d0c
oarinv <heizory@yandex.com>
2025-01-30 21:38:45 +0800

    change

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 # 安装所需软件包
 sudo apt update
-sudo apt install -y git ffmpeg bash-completion vim curl unzip wget  yt-dlp ssh clang build-essential
+sudo apt upgrade
+sudo apt install -y git ffmpeg bash-completion vim curl unzip wget  yt-dlp ssh clang build-essential 
 
-# 向/etc/wsl.conf文件中添加配置
-echo -e "[boot]\nsystemd=true" | sudo tee -a /etc/wsl.conf 
+# 安装rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-
+sudo apt install libssl-dev
